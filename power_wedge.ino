2f6b04cc9d409eed3fcdbe8aaccd34c6e06f8491
Cameron Layne <cameron.layne@gmail.com>
2022-03-25 15:53:08 -0700

    Update version number to model 1 firmware 1.

--- power_wedge.ino
+++ power_wedge.ino
@@ -103,8 +103,8 @@ const uint16_t angleUIChars[] = {
 
 // ----------------------------------------------------------
 
-#define modelVersion 0
-#define firmwareVersion 0
+#define modelVersion 1
+#define firmwareVersion 1
 
 // #define ENABLE_SERIAL_LOG
 #define ENABLE_SDCARD
